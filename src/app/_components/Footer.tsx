5cbb387f9ab04d5e2afdaf3b7f97b8747e2407b1
scorpion963 <jackson20062008@gmail.com>
2025-06-10 23:12:13 -0500

    kelv-inn

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -7,7 +7,7 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 export default async function Footer() {
   return (
     <div className='py-24 flex items-center justify-between px-4 xl:px-0'>
-      <h3 className='text-3xl font-bold cursor-pointer'>Elysian Stay</h3>
+      <h3 className='text-3xl font-bold cursor-pointer'>Kelv-Inn</h3>
 
       <div className='flex gap-2'>
         <FaInstagram size={24} className='cursor-pointer' />
--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Nav() {
   return (
-    <nav className='flex font-bold w-full justify-between mx-auto items-center bg-transparent'>
-        <h1 className='text-3xl cursor-pointer'>Elysian Stay</h1>
-        <div className='flex gap-6 text-xl font-bold'>
-            <Link  href="https://www.youtube.com/">INSTALL</Link>
-        </div>
+    <nav className="flex font-bold w-full justify-between mx-auto items-center bg-transparent">
+      <h1 className="text-3xl cursor-pointer">Kelv-Inn</h1>
+      <div className="flex gap-6 text-xl font-bold">
+        <Link
+          href="https://github.com/Scorpion963/Hotel_Management_System_1/releases/download/v0.0.2/Kelv-Inn-1.0.exe
+"
+        >
+          INSTALL
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
